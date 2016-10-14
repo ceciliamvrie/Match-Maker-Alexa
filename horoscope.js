e360ef848486ed25228d5516dcc531baba57f8ca
Cecilia Cisneros <cmvrie@utexas.edu>
2016-10-13 22:43:31 -0500

    horoscope uploaded

---
+++ horoscope.js
@@ -0,0 +1,24 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = {
+  "aries":"You live your relationships based on sizzling heat and fiery passion." + "Your relationships can have a mind of their own, nothing can hold it back from a night of entertainment.",
+  "taurus":"You enjoy things slow and sensual, especially when it comes to finding a partner." + "As a deep thinker, you know exactly what your looking for in a relationship.",
+  "gemini":"A decent challenge between the minds is exactly what you value in a partner." + "For you, a stimulating and interesting debate makes for the best aphrodisiac.",
+  "cancer":"When it comes to relationships, you prefer to have a connection with your partner more than anything." + "Having a person there to comfortably share your feelings with beats everything else.",
+  "leo":"In relationships, you like to be both the entertainer and the audience." + "To you, a boring partner is as deadly is the kiss of death.",
+  "virgo":"As a thinker, you can be analytical, thoughtful, and at times, moody." + "But that does not mean that your bad at relationships!" + "You need a partner that can show you exactly how much they care.",
+  "libra":"As a social romantic, you are always thinking of creative ways to show your partner exactly how much you love them." + "With you as a partner, there is no such thing as a boring relationship.",
+  "scorpio":"Your intense nature demands a deep and powerful connection with your love interest." + "Superficiality is a turn-off, as you want to dive beneath the surface and explore your partner's mysterious side.",
+  "sagittarius":"As an exciting and enthusiastic person, diversity and adventure are a must for you." + "You do best in a relationship where you can have plenty of space to roam free and explore unknown territories.",
+  "capricorn": "You are a committed lover that does best expressing your affection through actions more than words." + "In relationships, your generosity makes it so theres nothing more comfortable than you providing to your partner.",
+  "aquarius": "You are an inventive and stimulating partner who always has something new and interesting to offer up your sleeve." + "All you need is just a little space for yourself and that is where all the majic happens.",
+  "pisces": "You have boundless love to give to a relationship." + "All you need is someone that allows you to safely open your heart without fear of getting hurt in the end."
+};
